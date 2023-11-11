4e230408484f21c231755fcac9a16171dbd07522
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-11 16:04:38 -0600

    Add AgentsPage and update RootLayout title

---
+++ src/app/agents/page.tsx
@@ -0,0 +1,5 @@
+const AgentsPage = () => {
+	return <></>
+}
+
+export default AgentsPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-	return <h1>Hello World!</h1>
+	return <></>
 }
 
 export default HomePage
