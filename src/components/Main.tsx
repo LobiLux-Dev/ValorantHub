af752081460de61c5814409612b0539e745d17b0
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-10 21:49:48 -0600

    Update project files

--- src/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@chakra-ui/react'
-
-export const Main = () => {
-	return (
-		<main>
-			<Text as={'h1'} fontSize={'5xl'} align={'center'}>In maintenance</Text>
-		</main>
-	)
-}
