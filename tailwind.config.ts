6c5e2e725138bdc010bf8b9adb0e750afbee6994
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-11 16:28:38 -0600

    Rearranged dependencies and added DaisyUI theme to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,30 @@
 import type { Config } from 'tailwindcss'
 
+import DaisyUI from 'daisyui'
+
 const config: Config = {
 	content: ['./src/pages/**/*.{ts,tsx,mdx}', './src/components/**/*.{ts,tsx,mdx}', './src/app/**/*.{ts,tsx,mdx}'],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#ff3d43',
+					secondary: '#c1c1c4',
+					accent: '#27272f',
+					neutral: '#8e8e93',
+					'base-100': '#32333d',
+					info: '#3498db',
+					success: '#27ae60',
+					warning: '#f39c12',
+					error: '#e74c3c',
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [DaisyUI],
 }
 
 export default config
