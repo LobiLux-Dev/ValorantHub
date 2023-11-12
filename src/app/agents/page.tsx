de667fa5fd81acac24602aea62395e5d8b5f6d95
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-12 03:23:29 -0600

    Update agent card layout and dimensions

--- src/app/agents/page.tsx
+++ src/app/agents/page.tsx
@@ -10,23 +10,23 @@ const AgentsPage = async () => {
 		<div className="container flex flex-wrap gap-5 justify-center mx-auto py-1">
 			{agents.map(({ background, description, displayIcon, displayName, role, uuid }) => (
 				<div
-					className=" bg-accent border border-black card card-compact hover:shadow-2xl rounded-lg shadow-slate-800 w-64"
+					className=" bg-accent border border-black card card-compact hover:shadow-2xl rounded-lg shadow-slate-800 w-80"
 					key={uuid}
 				>
 					<Image
 						alt={displayName}
-						height={256}
+						height={320}
 						priority
 						src={displayIcon}
 						style={{ backgroundImage: `url(${background})` }}
-						width={256}
+						width={320}
 					/>
 					<div className="card-body">
 						<h3 className="card-title font-bold text-primary">{displayName}</h3>
 						<p className="text-secondary selection:bg-info/[.6] selection:text-accent">{description}</p>
 						<div className="divider m-0"></div>
-						<div className="flex gap-2">
-							<Image alt={role.displayName} height={24} src={role.displayIcon} width={24} />
+						<div className="flex gap-2 items-center">
+							<Image alt={role.displayName} height={30} src={role.displayIcon} width={30} />
 							<h5 className="font-semibold text-info">{role.displayName}</h5>
 						</div>
 					</div>
